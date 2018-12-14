643cdc1dd8e4a87674fff7e9a2c98934342393d8
刘少杰 <439655668@qq.com>
2018-12-14 16:49:43 +0800

    [modify]demo

--- app/main.js
+++ app/main.js
@@ -6,43 +6,51 @@ const init = () => {
     name: "store2"
   });
   console.log(store1);
-}
+};
 
 const jsTest = () => {
-  const img = document.querySelector('#targetImage');
-  img.style.position = 'fixed'
+  const img = document.querySelector("#targetImage");
+  img.style.position = "fixed";
   let top = 0;
   const range = 200;
   setInterval(() => {
     if (top > range) {
-      top = 0
+      top = 0;
     } else {
-      top++
+      top++;
     }
 
     if (top > range / 2) {
-      img.style.top = `${range - top}px`
+      img.style.top = `${range - top}px`;
     } else {
-      img.style.top = `${top}px`
+      img.style.top = `${top}px`;
     }
-  }, 30)
+  }, 30);
 };
 
 const main = () => {
-  console.log(localforage)
+  console.log(localforage);
   init();
   jsTest();
 
-  store1.setItem('key', 'value').then(v => console.log(v)).catch(e => console.error(e));
-  store1.getItem('key').then(v => console.log(v)).catch(e => console.error(e));
-
-  Array.from({
-    length: 9999
-  }).forEach((v, i) => localStorage.setItem(String(i), i))
+  store1
+    .setItem("key", "value")
+    .then(v => console.log(v))
+    .catch(e => console.error(e));
+  store1
+    .getItem("key")
+    .then(v => console.log(v))
+    .catch(e => console.error(e));
 
   // Array.from({
   //   length: 9999
-  // }).forEach((v, i) => store1.setItem(String(i), i))
+  // }).forEach((v, i) => localStorage.setItem(String(i), i));
+  // console.log("finish");
+
+  // Array.from({
+  //   length: 9999
+  // }).forEach((v, i) => store1.setItem(String(i), i));
+  // console.log("finish");
 };
 
 main();
