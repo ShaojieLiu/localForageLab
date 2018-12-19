f00d7d59f60d7133d8a34cc8cdeedd70b3fe5f68
刘少杰 <439655668@qq.com>
2018-12-19 14:42:40 +0800

    两种不同的文本存储空间不同

---
+++ app/data.js
@@ -0,0 +1,3 @@
+window.data = Array.from({ length: 1024 * 1024 })
+  .map(() => Math.floor(Math.random() * 10))
+  .join("");
--- app/main.js
+++ app/main.js
@@ -1,46 +1,51 @@
 let store1, store2;
 
 const init = () => {
+  const name = "store1";
   localforage.config();
   store1 = localforage.createInstance({
-    name: "store2"
+    name
   });
-  console.log(store1);
+  console.log(name, store1, window.data);
 };
 
-const jsTest = () => {
-  const img = document.querySelector("#targetImage");
-  img.style.position = "fixed";
-  let top = 0;
-  const range = 200;
-  setInterval(() => {
-    if (top > range) {
-      top = 0;
-    } else {
-      top++;
-    }
+// const jsTest = () => {
+//   const img = document.querySelector("#targetImage");
+//   img.style.position = "fixed";
+//   let top = 0;
+//   const range = 200;
+//   setInterval(() => {
+//     if (top > range) {
+//       top = 0;
+//     } else {
+//       top++;
+//     }
 
-    if (top > range / 2) {
-      img.style.top = `${range - top}px`;
-    } else {
-      img.style.top = `${top}px`;
-    }
-  }, 30);
-};
+//     if (top > range / 2) {
+//       img.style.top = `${range - top}px`;
+//     } else {
+//       img.style.top = `${top}px`;
+//     }
+//   }, 30);
+// };
 
 const main = () => {
-  console.log(localforage);
   init();
-  jsTest();
+  // jsTest();
 
-  store1
-    .setItem("key", "value")
-    .then(v => console.log(v))
-    .catch(e => console.error(e));
-  store1
-    .getItem("key")
-    .then(v => console.log(v))
-    .catch(e => console.error(e));
+  const setData = key => {
+    store1
+      .setItem(key.toString(), window.data)
+      .then(v => console.log("success"))
+      .catch(e => console.error(e));
+  };
+
+  // Array.from({ length: 1024 }).map((v, i) => setData(i));
+
+  // store1
+  //   .getItem("key")
+  //   .then(v => console.log(v))
+  //   .catch(e => console.error(e));
 
   // Array.from({
   //   length: 9999
